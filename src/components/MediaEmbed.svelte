d26b0a5a4f920466ca95c9cbc4c2786c123e6210
Faizal Chan. <moefc32@gmail.com>
2026-08-30 05:06:57 +0700

    edit resource files directory structure

--- src/components/MediaEmbed.svelte
+++ src/components/MediaEmbed.svelte
@@ -35,30 +35,30 @@
             <a
                 href={item.url}
                 target="_blank"
-                class="tooltip lg:tooltip-bottom lg:tooltip-end inline-block p-2 bg-slate-400/85 hover:bg-sky-800 border-2 border-slate-400/85 hover:border-black/20 transition rounded-2xl"
+                class="tooltip lg:tooltip-bottom lg:tooltip-end inline-block p-2 bg-slate-400/85 hover:bg-sky-800 border-2 border-slate-400/85 hover:border-black/20 transition rounded-2xl shadow-lg"
                 data-tip={item.name}
             >
                 <span
                     class="block bg-cover bg-center bg-no-repeat w-8 invert aspect-square"
-                    style={`background-image: url(icon/${item.class}.svg);`}
+                    style={`background-image: url(/res/icon/${item.class}.svg);`}
                 ></span>
             </a>
         {/each}
     </div>
 
     <div
-        class="hidden lg:flex gap-3 me-56 mb-4 pb-2 w-[calc(100vw-872px)] h-38 absolute right-0 bottom-0 overflow-x-auto rounded-lg"
+        class="hidden lg:flex gap-3 me-54 mb-4 px-2 py-2 w-[calc(100vw-856px)] h-40 absolute right-0 bottom-0 overflow-x-auto rounded-lg"
     >
         {#each youtube as item, i}
             <a
                 href={item.url}
                 target="_blank"
-                class="bg-gray-500 bg-cover bg-center bg-no-repeat h-full border-1 border-gray-600 aspect-video rounded-lg relative"
+                class="bg-gray-500 bg-cover bg-center bg-no-repeat h-full border-1 border-gray-600 aspect-video rounded-lg shadow-lg relative"
                 style={`background-image: url(${item.image});`}
                 title={item.title}
             >
                 <span
-                    class="bg-[url(icon/youtube.svg)] bg-cover bg-center bg-no-repeat w-[12.5%] aspect-square invert opacity-75 absolute left-2.5 bottom-2"
+                    class="bg-[url(/res/icon/youtube.svg)] bg-cover bg-center bg-no-repeat w-[12.5%] aspect-square invert opacity-75 absolute left-2.5 bottom-2"
                 ></span>
             </a>
         {/each}
