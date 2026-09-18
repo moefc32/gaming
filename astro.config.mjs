bfa75fde2c46302f8a4076bd302eb7b8806e6928
Faizal Chan. <moefc32@gmail.com>
2026-09-19 05:12:06 +0700

    upgrade Astro to v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-import { setMaxListeners } from 'events';
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import AstroPWA from '@vite-pwa/astro';
-
-setMaxListeners(12);
 
 export default defineConfig({
     vite: {
